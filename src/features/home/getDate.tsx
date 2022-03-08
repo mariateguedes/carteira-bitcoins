1d03d5a57aa665a267a58f345790ee8e6bfbd528
Maria Tereza Guedes <62268845+mariateguedes@users.noreply.github.com>
2022-03-08 10:39:13 -0300

    ajustando getDate

--- src/features/home/getDate.tsx
+++ src/features/home/getDate.tsx
@@ -4,7 +4,7 @@ export function getDate() {
     let day = today.getDate();
     if (dayOfTheWeek == 1) day = today.getDate() - 3;
     if (dayOfTheWeek == 0) day = today.getDate() - 2;
-    if (dayOfTheWeek == 7) day = today.getDate() - 1;
+    else day = today.getDate() - 1;
   
     return (today.getMonth() + 1) + '-' + day + '-' + today.getFullYear();
 }
